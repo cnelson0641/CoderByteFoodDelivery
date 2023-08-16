d509c05dac12d52a1a4c2a7f5b1ee77ce1ddb119
Chris Nelson <cnelson0641@gmail.com>
2023-08-16 15:08:27 -0400

    Added stubs for tests.

---
+++ lib/cbfd-app/src/test/java/cnelson0641/cbfd/OrderTest.java
@@ -0,0 +1,31 @@
+package cnelson0641.cbfd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import cnelson0641.cbfd.Order;
+
+public class OrderTest
+{
+    @Test
+    public void testConstructor()
+    {
+        //TODO create order and verify no failure
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGetters()
+    {
+        //TODO test the getters
+        assertTrue(true);
+    }
+
+    @Test
+    public void testSetters()
+    {
+        //TODO test the setters
+        assertTrue(true);
+    }
+}
---
+++ lib/cbfd-app/src/test/java/cnelson0641/cbfd/PaymentTest.java
@@ -0,0 +1,17 @@
+package cnelson0641.cbfd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import cnelson0641.cbfd.Payment;
+
+public class PaymentTest
+{
+    @Test
+    public void testConstructor()
+    {
+        //TODO create payment and verify no failure
+        assertTrue(true);
+    }
+}
